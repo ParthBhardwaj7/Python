90783baf409f4c3d9c778a37d10bd91919cdc2f4
Parth <parthbhardwaj2579@gmail.com>
2025-01-31 22:18:39 +0530

    initial commit

---
+++ coding/day4.py
@@ -0,0 +1,32 @@
+# Jump Statement
+# Break statement which uses break 
+n=23
+i=1
+while i<n:
+    print(i)
+    if i==5:
+        print ("breaking")
+    i+=1
+n=23
+i=1
+while i<n:
+    print(i)
+    if i==5:
+        print ("breaking")
+    i+=1
+
+#Continue statement
+for letter in "python":
+    if letter=="h":
+        continue
+    print(letter)
+
+#Syntax of If Statement
+# Syntax of the if Statement
+# if expression:
+# statement(s) to be executed
+x=10
+if x==10:
+    print("hehe")
+else:
+    print(hohho)
