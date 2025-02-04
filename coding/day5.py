aa888fa9b296b7b358bea615a4544733e26b9e3b
Parth <parthbhardwaj2579@gmail.com>
2025-02-04 22:43:07 +0530

    day10

--- coding/day5.py
+++ coding/day5.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Define a lambda function to add two numbers
 add = lambda a, b: a + b
 
@@ -19,3 +20,23 @@ numbers = [1, 2, 3, 4, 5]
 evens = list(filter(lambda x: x % 2 == 0, numbers))
 
 print(evens)  # Output: [2, 4]
+=======
+parth={ 1:"parth",2:"partahs" , 3:"sdsd"}
+for i in parth:
+    print(i) #Return keys
+
+for i in parth:
+    print(parth[i])# return values
+
+#Return Keys
+for i in parth.keys():
+    print(i)
+
+#Return Values
+for i in parth.values():
+    print(i)
+
+#Return both
+for i,y in parth():
+    print(i,y)
+>>>>>>> 19f018e (day10)
